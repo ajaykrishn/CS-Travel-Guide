37307a1983c1a83184ee695e87c15c09483e5f0c
Ajay-Krishnan-Balal <88720459+Ajay-Krishnan-Balal@users.noreply.github.com>
2021-08-26 17:03:45 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,84 @@
+import webbrowser               #built-in module
+import wikipedia                 #install by : pip install wikipedia
+from PIL import Image           #install by : pip install Pillow
+from review_mod import *             #program module
+import mysql.connector as mysql  #install by : pip install mysql-connector-python
+import time                   #built-in module
+
+welcome()
+
+con_check=1
+while con_check:
+    try:
+        usr=input("Enter Username: ")
+        psw=input("Enter Password: ")
+        con=mysql.connect(host='localhost',user=usr,passwd=psw)
+        curs=con.cursor()
+        create_dbase(curs)
+        print("WELCOME!")
+        con_check=0
+    except Exception as e:
+        print(e)
+        print("Please try again.\n")
+
+op2=1
+print("Choose an option from the below listed : \n 1. Information of a place \n 2. Hotel Bookings \n 3. Add reviews for a place ")
+print("4. Exit")
+
+while op2:
+    q=int(input("\n\nYour choice (1,2,3,4) : "))
+
+    if q==1:
+        try:
+            infoplace=input("Enter the place you would like to know about : ")
+            result=wikipedia.summary(infoplace, sentences=5)
+            print(result)
+            print("Reviews:")
+            show_reviews(con,infoplace)
+            ch=input("Do you want to add review?(y/n): ")
+            if ch.lower()=="y":
+                create(con,infoplace)
+            print()
+        except:
+            print("Place not available")
+        time.sleep(2)
+
+    elif q==2:
+        print('\n\nService is currently only supporting hotels in Kerala \nYou will be redirected to an external website')
+        print("Please enter information asked below :")
+        place=input("\nEnter Destination : ")
+        adults=int(input("Enter total number of Adults : "))
+        children=int(input("enter total number of children : "))
+        x=("https://www.yatra.com/pwa/hotels/srp?roomRequests[0].id=1&roomRequests[0].noOfAdults={}&roomRequests[0].noOfChildren={}&source=BOOKING_ENGINE&pg=1&tenant=B2C&isPersnldSrp=1&city.name={}&city.code={}&state.name=KER&state.code=KER&country.name=India&country.code=IND".format(adults,children,place,place))
+        webbrowser.open(x, new=1)
+        time.sleep(2)
+
+    elif q==3:
+        print("Enter a :To write a review \nEnter b :To edit a review \nEnter c : To delete a review \nEnter d : Exit From this option.")
+        op3=1
+        while op3:
+            op4=input("\n\nYour choice (a,b,c,d) : ")
+            if op4=='a':
+                p=input("Enter the Place: ")
+                create(con,p)
+            elif op4=="b":
+                edit(con)
+            elif op4=="c":
+                delete(con)
+            elif op4=="d":
+                op3=0
+            else:
+                print("Kindly enter a valid option")
+            time.sleep(1)
+        time.sleep(2)
+    elif q==4:
+        op2=0
+
+    else:
+        print("Please enter a valid choice.")
+
+curs.close()
+con.close()
+
+print("Thank you for using our Service.")
+time.sleep(3)
---
+++ review_mod.py
@@ -0,0 +1,71 @@
+def welcome(): #welcome message
+    pass
+
+def create_dbase(curs):
+    curs.execute("SELECT count(*) FROM information_schema.TABLES WHERE (TABLE_SCHEMA = 'Review') AND (TABLE_NAME = 'Reviews')")
+    c=curs.fetchone()[0]
+    if c==0:
+        curs.execute("Create Database Review")
+        curs.execute("Use Review")
+        curs.execute("Create table Reviews(rev_id int primary key,usr_name varchar(30),Place varchar(50),Reviews varchar(250))")
+        curs.execute("Insert into Reviews values(1000,'admin','test','test_review')")
+    else:
+        curs.execute("Use Review")
+
+def create(conn,place):   #for writing reviews
+    f=2
+    curs=conn.cursor()
+    while f!=1:
+        m=input('Write your review here: ')
+        name=input("Enter Your name: ")
+        curs.execute("SELECT max(rev_id) FROM Reviews")
+        n=curs.fetchone()[0]
+        print("Reference id for editing or deleting your review is: ",n+1)
+        t=(n+1,name,place,m)
+        s='insert into Reviews values(%s,%s,%s,%s)'
+        curs.execute(s,t)
+        conn.commit()
+        f=int(input('Enter zero to quit '))
+        f=f+1
+    curs.close()
+
+def show_reviews(conn,place):  #for displaying reviews
+	curs=conn.cursor()
+	curs.execute("SELECT usr_name,REVIEWS FROM Reviews WHERE Place=%s",(place,))
+	rev=curs.fetchall()
+	if rev:
+		for i in rev:
+			print(i[0],':',i[1])
+	else:
+		print("No Reviews were Found.")
+	curs.close()
+
+def edit(conn):     #for editing reviews previously entered
+    curs=conn.cursor()
+    a=int(input("Enter Review Id for the review you would like to edit: "))
+    b=input("Enter new review: ")
+    s=("Update Reviews set Reviews=%s where rev_id=%s")
+    curs.execute(s,(b,a))
+    if curs.rowcount==0:
+        print("The reference id entered does not exist.Enter a valid id")
+    else:
+        print("Review has been updated")
+    conn.commit()
+    curs.close()
+
+
+def delete(conn): #for deleting reviews previously entered
+    curs=conn.cursor()
+    d=int(input("Enter the reference id for the review you would like to delete: "))
+    s=('Delete from Reviews where rev_id=%s')
+    curs.execute(s,(d,))
+    if curs.rowcount==0:
+          print("The reference id entered does not exist.Enter a valid id.")
+    else:
+        print("Review has been deleted")
+    conn.commit()
+    curs.close()
+
+
+
+	
