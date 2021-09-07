148ec61653e4bcc930f71a722f5cd5910f6e8e05
Ajay-Krishnan-Balal <88720459+Ajay-Krishnan-Balal@users.noreply.github.com>
2021-09-07 10:00:16 +0530

    Update review_mod.py

--- review_mod.py
+++ review_mod.py
@@ -1,5 +1,3 @@
-import os
-
 def welcome(): #welcome message
     print('\t\t\t\tWELCOME! \n\nThis program has been designed to help you find information regarding places \nWe hope this will be of help to you in finding what you are looking for! \n\t\t\t\t\t\t\t\t\tᴹᵃᵈᵉ ᵇʸ ᴬʲᵃʸ,ᴬᵐᵘʳᵗʰᵃ,ᴬʸᵈᶦⁿ\n')
 
