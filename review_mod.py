bf8a197464208d9232766bf71012d460097a8408
Amrutha0401 <88905004+Amrutha0401@users.noreply.github.com>
2021-09-08 00:34:15 -0700

    Update review_mod.py

--- review_mod.py
+++ review_mod.py
@@ -1,5 +1,5 @@
 def welcome(): #welcome message
-    print('\t\t\t\tWELCOME! \n\nThis program has been designed to help you find information regarding places \nWe hope this will be of help to you in finding what you are looking for! \n\t\t\t\t\t\t\t\t\tᴹᵃᵈᵉ ᵇʸ ᴬʲᵃʸ,ᴬᵐᵘʳᵗʰᵃ,ᴬʸᵈᶦⁿ\n')
+    print('\t\t\t\tWELCOME! \n\nThis program has been designed to help you find information regarding places \nWe hope this will be of help to you in finding what you are looking for!")
 
 def create(conn,place):   #for writing reviews
     f=2
