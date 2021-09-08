d7eab5fb72a0885ca271afae5c7d879b9d15f59a
Ajay-Krishnan-Balal <88720459+Ajay-Krishnan-Balal@users.noreply.github.com>
2021-09-08 13:23:39 +0530

    Update Wikipedia_install.py

--- Wikipedia_install.py
+++ Wikipedia_install.py
@@ -1,4 +1,15 @@
-#pip install wikipedia module
-import sys
+"""   This module will install Wikipedia module using pip.
+     To install wikipedia module run this code."""
+import sys,time
 import subprocess
 subprocess.check_call([sys.executable, '-m', 'pip', 'install','wikipedia'])
+
+#To check if installed
+reqs = subprocess.check_output([sys.executable, '-m', 'pip','freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+if "wikipedia" in installed_packages:
+    print("Wikipedia installed Successfully")
+else:
+    print("Unsuccessful")
+time.sleep(0.7)
+  
