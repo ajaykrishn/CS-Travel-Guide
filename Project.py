d250e86e99bc7b00cc0f9de03a3076ff15984cd1
Ajay-Krishnan-Balal <88720459+Ajay-Krishnan-Balal@users.noreply.github.com>
2021-09-09 10:52:40 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -5,30 +5,26 @@ from dtbs_mod import *        #program module
 try:
     import wikipedia                 # by pip install wikipedia
 except:
-    inst_wiki()
+    inst_wiki()                 #option to install module
     import wikipedia
 
 welcome()
 print("Please Enter your Mysql Credintials to continue:")
 
-con_check=1
-while con_check:
+while True:
     try:
         con=connectdb()
-        con_check=0
+        break
     except Exception as e:
         print(e)
-        print("Please try again.\n")
+        print("Something went wrong.Please try again.\n")
 os.system("cls")
 
-
-op2=1
-
-while op2:
+while True:
     print('\n\t\tMain Menu\n\nChoose an option from the below listed : \n\n 1. Information of place \n 2. Hotel bookings \n 3. Reviews \n 4. Exit ')
-    q=int(input("\n\nYour choice (1,2,3,4) : "))
+    q=input("\n\nYour choice (1,2,3,4) : ")
 
-    if q==1:
+    if q=='1':
         try:
             infoplace=input("Enter the place you would like to know about : ")#view reviews
             result=wikipedia.summary(infoplace, sentences=5)
@@ -43,54 +39,52 @@ while op2:
             print()
         except Exception as e:
             print("Error code:",e)
-        time.sleep(2)
+        time.sleep(0.5)
 
-    elif q==2:
-        bk=1
+    elif q=='2':
         print("\n\nEnter the website you would like to do your hotel booking in : \n 1. Yatra\n 2. Easemytrip\n 3. Exit \n\n")
-        while bk:
+        while True:
             choice=input("Your response :")
             if choice=="1":
                 booking.yatra()
-                bk=0
+                break
             elif choice=="2":
                 booking.easemytrip()
-                bk=0
+                break
             elif choice=="3":
-                bk=0
+                break
             else:
                 print("Sorry , please enter a valid option")
-                time.sleep(1)
+                time.sleep(0.5)
 
 
-    elif q==3:
-        op3=1
-        while op3:
+    elif q=='3':
+        while True:
             print("\n\nEnter a : To write review \nEnter b : To edit a review \nEnter c : To delete a review \nEnter d : To view previous reviews \nEnter e : Exit this option")
             op4=input("\n\nYour choice (a,b,c,d,e) : ")
-            if op4=='a':
+            if op4.lower()=='a':
                 p=input("Enter the Place: ")
                 create(con,p)
                 op3=0
-            elif op4=="b":
+            elif op4.lower()=="b":
                 edit(con)
                 op3=0
-            elif op4=="c":
+            elif op4.lower()=="c":
                 delete(con)
                 op3=0
-            elif op4=="d":
+            elif op4.lower()=="d":
                 id=int(input("Enter the reference id"))
                 show_reviews(con,id)
                 time.sleep(1)
                 op3=0
-            elif op4=='e':
-                op3=0
+            elif op4.lower()=='e':
+                break
             else:
                 print("Kindly enter a valid option")
                 time.sleep(0.5)
         time.sleep(0.7)
-    elif q==4:
-        op2=0
+    elif q=='4':
+        break
 
     else:
         print("Please enter a valid choice.")
@@ -98,5 +92,4 @@ while op2:
 con.close()
 
 print("Thank you for using our service")
-print("\t\t\tThis program has been created by Ajay Krishnan and team")
 time.sleep(1.5)
